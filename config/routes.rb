0c0dc4cdd00c087d0c9256e69a618e1d99c4f8c2
Toshihiko Kubo <migraine1031@hotmail.com>
2016-02-20 21:43:19 +0900

    created buyer and retailer model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+ devise_for :buyers, controllers: {
+  sessions:      'buyers/sessions',
+  passwords:     'buyers/passwords',
+  registrations: 'buyers/registrations'
+}
+devise_for :retailers, controllers: {
+  sessions:      'retailers/sessions',
+  passwords:     'retailers/passwords',
+  registrations: 'retailers/registrations'
+}
+
+  # devise_for :retailers
+  # devise_for :buyers
 root to: "top#index"
 end
