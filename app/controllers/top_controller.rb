df6c0dcd375c10b70b6a4f5142fe9129fae42ac3
Toshihiko Kubo <migraine1031@hotmail.com>
2016-02-23 12:03:31 +0900

    諸々の変更点

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,11 @@
 class TopController < ApplicationController
+
+  before_action :move_to_content_page
+
   def index
+  end
+
+  def move_to_content_page
+      redirect_to contents_url unless retailer_signed_in? || buyer_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
- devise_for :buyers, controllers: {
-  sessions:      'buyers/sessions',
-  passwords:     'buyers/passwords',
-  registrations: 'buyers/registrations'
-}
-devise_for :retailers, controllers: {
-  sessions:      'retailers/sessions',
-  passwords:     'retailers/passwords',
-  registrations: 'retailers/registrations'
-}
-
   # devise_for :retailers
   # devise_for :buyers
-root to: "top#index"
+resources :contents, only: [:index]
+resources :users, only: [:show]
+root to: "contents#index"
 end
