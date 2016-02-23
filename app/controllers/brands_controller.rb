5c9a09a2497112a15f0e6ad3425f2bfa659749ee
Toshihiko Kubo <migraine1031@hotmail.com>
2016-02-23 19:32:00 +0900

    update機能完了

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -8,6 +8,12 @@ class BrandsController < ApplicationController
   end
 
   def update
+    current_brand.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:brand).permit(:company_name, :company_address, :country, :user_name, :phone_number, :item_type)
   end
 
 end
--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -7,5 +7,11 @@ class RetailersController < ApplicationController
   end
 
   def update
+    current_retailer.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:retailer).permit(:company_name, :company_address, :country, :user_name, :phone_number, :item_type)
   end
 end
